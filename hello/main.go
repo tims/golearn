65785c814bc1eb8d663d8914452ca0b98c56f555
Tim Sell <trsell@gmail.com>
2015-06-29 17:22:17 -0400

    web server with critical information

--- hello/main.go
+++ hello/main.go
@@ -1,9 +1,33 @@
 package main
-import "fmt"
-import "github.com/tims/golearn/utils"
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"encoding/json"
+	"log"
+)
+
+type Salt struct {
+	Text string `json:"text"`
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	salt := Salt{"Around and around the rugged rucks the ragged rascal ran"}
+	fmt.Printf("%+v\n", *r)
+
+	// What are you requesting? wat?
+	b, err := json.Marshal(salt)
+	if err != nil {
+		log.Fatal(err);
+		io.WriteString(w, "arg")
+	} else {
+		io.WriteString(w, string(b))
+	}
+}
 
 func main() {
-	fmt.Println("Hi, I am made of cheese");
-
-	fmt.Println(utils.Reverse("Hi, I am made of cheese"));
+	fmt.Println("Hi, I am made of cheese")
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8000", nil)
 }
