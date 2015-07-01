73b912b93e796b3487c248ec31d80c6ec541e6ba
Tim Sell <trsell@gmail.com>
2015-06-30 22:25:28 -0400

    experiments

--- src/hello/main.go
+++ src/hello/main.go
@@ -26,8 +26,25 @@ func index(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func print(c chan int) {
+	fmt.Println(<- c);
+}
+
 func main() {
 	fmt.Println("Hi, I am made of cheese")
-	http.HandleFunc("/", index)
-	http.ListenAndServe(":8000", nil)
+	//http.HandleFunc("/", index)
+	//http.ListenAndServe(":8000", nil)
+
+	var c = make(chan string);
+	for i := 1; i <= 5; i++ {
+		go func(i int, co chan<- string) {
+			for j := 1; j <= 5; j++ {
+				co <- fmt.Sprintf("hi from %d.%d", i, j)
+			}
+		}(i, c)
+	}
+
+	for i := 1; i <= 25; i++ {
+		fmt.Println(<-c)
+	}
 }
